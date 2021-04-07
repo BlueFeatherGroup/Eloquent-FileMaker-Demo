e8e2c487c3e4a7adf65f2f40060b8c29e50eb96a
David Nahodyl <david@bluefeathergroup.com>
2021-04-07 18:24:37 -0400

    added new directory to purge, jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
+    mode: 'jit',
     purge: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './storage/framework/views/*.php'
     ],  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
