a362a53ff2f06845e12886df593052432f2dcd7f
David Nahodyl <david@bluefeathergroup.com>
2021-09-02 12:20:19 -0400

    changed dash to underscore

--- resources/views/person-detail.blade.php
+++ resources/views/person-detail.blade.php
@@ -30,24 +30,24 @@
         <form class="w-full max-w-sm">
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name-first"> First
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name_first"> First
                         Name </label>
                 </div>
                 <div class="md:w-2/3">
                     <input
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        name="name-first" type="text" value="David">
+                        name="name_first" type="text" value="David">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name-last"> Last
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name_last"> Last
                         Name </label>
                 </div>
                 <div class="md:w-2/3">
                     <input
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        name="name-last" type="text" value="Nahodyl">
+                        name="name_last" type="text" value="Nahodyl">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
