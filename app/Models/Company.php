9294d0b08bbd50039c9a981c9495613085b36aed
David Nahodyl <david@bluefeathergroup.com>
2021-09-02 01:15:16 -0400

    working on it

---
+++ app/Models/Company.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use BlueFeather\EloquentFileMaker\Database\Eloquent\FMModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends FMModel
+{
+    use HasFactory;
+}
---
+++ app/Models/Person.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use BlueFeather\EloquentFileMaker\Database\Eloquent\FMModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends FMModel
+{
+    use HasFactory;
+
+    public function company(){
+        return $this->hasOne(Company::class);
+    }
+}
